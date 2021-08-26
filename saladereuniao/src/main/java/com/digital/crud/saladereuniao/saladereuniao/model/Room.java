267361acae1fd4b9ba2b206a9627ed9297c85afe
Pbugui <pbugui@gmail.com>
2021-08-26 16:22:09 -0300

    Back-end

---
+++ saladereuniao/src/main/java/com/digital/crud/saladereuniao/saladereuniao/model/Room.java
@@ -0,0 +1,30 @@
+package com.digital.crud.saladereuniao.saladereuniao.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@ToString
+@Builder
+@Entity
+@Table(name = "meetingroom")
+public class Room {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column(name = "name", nullable = false)
+    private String name;
+    @Column(name = "date", nullable = false)
+    private String date;
+    @Column(name = "startHour", nullable = false)
+    private String startHour;
+    @Column(name = "endHour", nullable = false)
+    private String endHour;
+
+}
